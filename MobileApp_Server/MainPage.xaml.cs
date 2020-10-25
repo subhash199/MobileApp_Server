5fcbb771ae1d56ee4a072a7a18c1a33263c3e52a
subhash venkatesan <blindskater97@gmail.com>
2020-10-25 18:19:30 +0000

    home button and orders are added. Ui is better

--- MobileApp_Server/MainPage.xaml.cs
+++ MobileApp_Server/MainPage.xaml.cs
@@ -12,6 +12,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+using Microsoft.Data.Sqlite;
+using Windows.ApplicationModel.VoiceCommands;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -23,21 +27,40 @@ namespace MobileApp_Server
     public sealed partial class MainPage : Page
     {
         private List<Products_Class> products = new List<Products_Class>();
+        private static string databasePath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "ProductData.db");
+        private SqliteConnection sqliteConnection = new SqliteConnection($"Filename={databasePath}");
         public MainPage()
         {
             this.InitializeComponent();
-           
-            
+            try
+            {
+
+                sqliteConnection.Open();
+            }
+            catch
+            {
+                CreateDatabase();
+            }
            
         }
 
-        private void ServerButton_Toggled(object sender, RoutedEventArgs e)
+        private async void CreateDatabase()
         {
+            
+                StorageFile file = await ApplicationData.Current.LocalFolder.CreateFileAsync("ProductData.db");
+            
+        }
 
+        private  void ServerButton_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (ServerButton.IsOn == true)
+            {
+
+            }
         }
 
         private void ItemAdd_Click(object sender, RoutedEventArgs e)
-        {           
+        {
             Frame.Navigate(typeof(AddItem_Page));
         }
     }
